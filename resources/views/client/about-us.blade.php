2d538fd9cfb6435972bd63a45b18c342956e985f
Dorsone <jasur.coder@gmail.com>
2022-04-25 15:42:08 +0500

    about us page created

---
+++ resources/views/client/about-us.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app-assets')
+
+@section('content')
+    @include('components.bread-crumbs')
+    @include('components.about-us')
+    @include('components.home-experience')
+    @include('components.parents-reviews')
+    @include('components.request-us')
+    @include('components.footer-gallery')
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('', function (){
     return view('client.index');
 });
+
+Route::get('about-us', function () {
+   return view('client.about-us');
+});

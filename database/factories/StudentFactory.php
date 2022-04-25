2ae2335534efeb187284680026335a582d6a8b98
Dorsone <jasur.coder@gmail.com>
2022-04-25 18:05:57 +0500

    student migration created

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Level;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class StudentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $level = Level::query()->inRandomOrder()->first();
+        return [
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'phone' => $this->faker->phoneNumber(),
+            'status' => $this->faker->boolean(),
+            'born' => $this->faker->date(),
+            'level_id' => $level->id,
+            'image' => $this->faker->imageUrl(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             ArticleSeeder::class,
             LevelSeeder::class,
             CourseSeeder::class,
+            StudentSeeder::class,
         ]);
     }
 }

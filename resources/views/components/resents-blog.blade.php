ddda153a30a19b735fe77573ba18af12656a4a1c
Dorsone <jasur.coder@gmail.com>
2022-04-26 17:32:38 +0500

    blog section created

--- resources/views/components/resents-blog.blade.php
+++ resources/views/components/resents-blog.blade.php
@@ -7,72 +7,29 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-md-6 col-lg-4 ftco-animate">
-                <div class="blog-entry">
-                    <a href="javascript:void(0)" class="block-20 d-flex align-items-end" style="background-image: url({{asset('images/image_1.jpg')}});">
-                        <div class="meta-date text-center p-2">
-                            <span class="day">27</span>
-                            <span class="mos">January</span>
-                            <span class="yr">2019</span>
-                        </div>
-                    </a>
-                    <div class="text bg-white p-4">
-                        <h3 class="heading"><a href="#">Skills To Develop Your Child Memory</a></h3>
-                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                        <div class="d-flex align-items-center mt-4">
-                            <p class="mb-0"><a href="#" class="btn btn-secondary">Read More <span class="ion-ios-arrow-round-forward"></span></a></p>
-                            <p class="ml-auto mb-0">
-                                <a href="#" class="mr-2">Admin</a>
-                                <a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-lg-4 ftco-animate">
-                <div class="blog-entry">
-                    <a href="javascript:void(0)" class="block-20 d-flex align-items-end" style="background-image: url({{asset('images/image_2.jpg')}});">
-                        <div class="meta-date text-center p-2">
-                            <span class="day">27</span>
-                            <span class="mos">January</span>
-                            <span class="yr">2019</span>
-                        </div>
-                    </a>
-                    <div class="text bg-white p-4">
-                        <h3 class="heading"><a href="#">Skills To Develop Your Child Memory</a></h3>
-                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                        <div class="d-flex align-items-center mt-4">
-                            <p class="mb-0"><a href="#" class="btn btn-secondary">Read More <span class="ion-ios-arrow-round-forward"></span></a></p>
-                            <p class="ml-auto mb-0">
-                                <a href="#" class="mr-2">Admin</a>
-                                <a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a>
-                            </p>
+            @foreach($articles as $article)
+                <div class="col-md-6 col-lg-4 ftco-animate">
+                    <div class="blog-entry">
+                        <a href="javascript:void(0)" class="block-20 d-flex align-items-end" style="background-image: url({{asset($article->image)}});">
+                            <div class="meta-date text-center p-2">
+                                <span class="day">{{$article->created_at->format('d')}}</span>
+                                <span class="mos">{{$article->created_at->format('M')}}</span>
+                                <span class="yr">{{$article->created_at->format('Y')}}</span>
+                            </div>
+                        </a>
+                        <div class="text bg-white p-4">
+                            <h3 class="heading"><a href="#">{{$article->title_preview_en}}</a></h3>
+                            <p style="min-height: 100px">{{$article->content_preview_en}}</p>
+                            <div class="d-flex align-items-center mt-4">
+                                <p class="mb-0"><a href="#" class="btn btn-secondary">Read More <span class="ion-ios-arrow-round-forward"></span></a></p>
+                                <p class="ml-auto mb-0">
+                                    <a href="#" class="mr-2">{{$article->user->name_en}}</a>
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-6 col-lg-4 ftco-animate">
-                <div class="blog-entry">
-                    <a href="javascript:void(0)" class="block-20 d-flex align-items-end" style="background-image: url({{asset("images/image_3.jpg")}});">
-                        <div class="meta-date text-center p-2">
-                            <span class="day">27</span>
-                            <span class="mos">January</span>
-                            <span class="yr">2019</span>
-                        </div>
-                    </a>
-                    <div class="text bg-white p-4">
-                        <h3 class="heading"><a href="#">Skills To Develop Your Child Memory</a></h3>
-                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                        <div class="d-flex align-items-center mt-4">
-                            <p class="mb-0"><a href="#" class="btn btn-secondary">Read More <span class="ion-ios-arrow-round-forward"></span></a></p>
-                            <p class="ml-auto mb-0">
-                                <a href="#" class="mr-2">Admin</a>
-                                <a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @endforeach
         </div>
     </div>
 </section>

ccf7c76a397e7b3333760940b26cc61cc1e937a1
Dorsone <jasur.coder@gmail.com>
2022-04-27 15:42:54 +0500

    mini-services translated

--- resources/views/components/about-us.blade.php
+++ resources/views/components/about-us.blade.php
@@ -5,7 +5,6 @@
                 <div class="text px-4 ftco-animate">
                     <h2 class="mb-4">{{__('about-us.Welcome_TO.title')}}</h2>
                     <p>{{__('about-us.Welcome_TO.body')}}</p>
-                    <p><a href="#" class="btn btn-secondary px-4 py-3">Read More</a></p>
                 </div>
             </div>
             <div class="col-md-7 wrap-about py-5 pr-md-4 ftco-animate">
@@ -16,8 +15,8 @@
                         <div class="services-2 d-flex">
                             <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-security"></span></div>
                             <div class="text">
-                                <h3>Safety First</h3>
-                                <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+                                <h3>{{__('mini-services.SAFETY_FIRST.title')}}</h3>
+                                <p>{{__('mini-services.SAFETY_FIRST.body')}}</p>
                             </div>
                         </div>
                     </div>
@@ -25,8 +24,8 @@
                         <div class="services-2 d-flex">
                             <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-reading"></span></div>
                             <div class="text">
-                                <h3>Regular Classes</h3>
-                                <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+                                <h3>{{__('mini-services.REGULAR_CLASSES.title')}}</h3>
+                                <p>{{__('mini-services.REGULAR_CLASSES.body')}}</p>
                             </div>
                         </div>
                     </div>
@@ -34,8 +33,8 @@
                         <div class="services-2 d-flex">
                             <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-diploma"></span></div>
                             <div class="text">
-                                <h3>Certified Teachers</h3>
-                                <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+                                <h3>{{__('mini-services.CERTIFIED_TEACHERS.title')}}</h3>
+                                <p>{{__('mini-services.CERTIFIED_TEACHERS.body')}}</p>
                             </div>
                         </div>
                     </div>
@@ -43,8 +42,8 @@
                         <div class="services-2 d-flex">
                             <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-education"></span></div>
                             <div class="text">
-                                <h3>Sufficient Classrooms</h3>
-                                <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+                                <h3>{{__('mini-services.SUFFICIENT_CLASSROOMS.title')}}</h3>
+                                <p>{{__('mini-services.SUFFICIENT_CLASSROOMS.body')}}</p>
                             </div>
                         </div>
                     </div>
@@ -52,8 +51,8 @@
                         <div class="services-2 d-flex">
                             <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-jigsaw"></span></div>
                             <div class="text">
-                                <h3>Creative Lessons</h3>
-                                <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+                                <h3>{{__('mini-services.CREATIVE_LESSONS.title')}}</h3>
+                                <p>{{__('mini-services.CREATIVE_LESSONS.body')}}</p>
                             </div>
                         </div>
                     </div>
@@ -61,8 +60,8 @@
                         <div class="services-2 d-flex">
                             <div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-kids"></span></div>
                             <div class="text">
-                                <h3>Sports Facilities</h3>
-                                <p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+                                <h3>{{__('mini-services.SPORTS_FACILITIES.title')}}</h3>
+                                <p>{{__('mini-services.SPORTS_FACILITIES.body')}}</p>
                             </div>
                         </div>
                     </div>

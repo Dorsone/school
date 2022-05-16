3429eb6a7a3817dffe558a9ceb5e519a12bfae5b
Dorsone <jasur.coder@gmail.com>
2022-05-16 13:41:12 +0500

    some changes

--- resources/lang/uz/mini-services.php
+++ resources/lang/uz/mini-services.php
@@ -3,8 +3,8 @@
 return [
     'SAFETY_FIRST' => ['title' => "Birinchi navbatda xavfsizlik", 'body' => "Hatto qudratli Ko'rsatkich ham ko'r matnlar ustidan hech qanday kuchga ega emas, u deyarli orfografikdir."],
     'REGULAR_CLASSES' => ['title' => "Doimiy darslar", 'body' => "Hatto qudratli Ko'rsatkich ham ko'r matnlar ustidan hech qanday kuchga ega emas, u deyarli orfografikdir."],
-    'CERTIFIED_TEACHERS' => ['title' => "Sertifikatlangan o'qituvchilar", 'body' => "Hatto qudratli Ko'rsatkich ham ko'r matnlar ustidan hech qanday kuchga ega emas, u deyarli orfografikdir."],
-    'SUFFICIENT_CLASSROOMS' => ['title' => "O'quv xonalari yetarli", 'body' => "Hatto qudratli Ko'rsatkich ham ko'r matnlar ustidan hech qanday kuchga ega emas, u deyarli orfografikdir."],
+    'CERTIFIED_TEACHERS' => ['title' => "Sertifikatlangan o'qituvchilar", 'body' => "Maktabda chet tili fanlaridan 5-ta ingliz tili va 3-ta rus tili xalqaro sertifikatlarni olgan ustozlar dars berib kelmoqda."],
+    'SUFFICIENT_CLASSROOMS' => ['title' => "O'quv xonalari yetarli", 'body' => "Maktabda 24 ta sinf bo'lib,har bir sinfda a'lochi, bilimli, iqtidorli,faol o'quvchilar juda ko'p."],
     'CREATIVE_LESSONS' => ['title' => 'Ijodiy darslar', 'body' => "Hatto qudratli Ko'rsatkich ham ko'r matnlar ustidan hech qanday kuchga ega emas, u deyarli orfografikdir."],
     'SPORTS_FACILITIES' => ['title' => 'Sport inshootlari', 'body' => "Hatto qudratli Ko'rsatkich ham ko'r matnlar ustidan hech qanday kuchga ega emas, u deyarli orfografikdir."],
 ];

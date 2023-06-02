e83d4c94cfe00e15f1626b2751b22851ec879582
Jasur Dusmurodov <d.jasur@mga.team>
2023-06-02 14:18:40 +0500

    fix

--- resources/views/components/home-experience.blade.php
+++ resources/views/components/home-experience.blade.php
@@ -13,7 +13,7 @@
                         <div class="block-18">
                             <div class="icon"><span class="flaticon-doctor"></span></div>
                             <div class="text">
-                                <strong class="number" data-number="{{$experiences->where('name', 'certified')->first()->value}}">0</strong>
+                                <strong class="number" data-number="{{$certified = $experiences->where('name', 'certified')->first() ? $certified->value : null}}">0</strong>
                                 <span>{{__('sections/experience.content.0')}}</span>
                             </div>
                         </div>
